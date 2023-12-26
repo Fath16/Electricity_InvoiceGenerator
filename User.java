2f61ef5ab696510c8c456742f796f853c9bcf155
Fath16 <106072698+Fath16@users.noreply.github.com>
2023-12-26 13:35:53 +0700

    Update User.java
    add comment

--- User.java
+++ User.java
@@ -1,14 +1,17 @@
 package Electricity_InvoiceGenerator;
 
+// Abstract class 
 public abstract class User {
     private String username;
     private String password;
 
+    // Constructor 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
+    // Getter methods 
     public String getUsername() {
         return username;
     }
@@ -17,26 +20,28 @@ public abstract class User {
         return password;
     }
 
+    // Override toString() to represent user information as a string
     @Override
     public String toString() {
         return "Username: " + username;
     }
 
+    // Override equals() and hashCode() for proper object comparison and hashing
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) // Check if the objects are the same
             return true;
-        if (obj == null || getClass() != obj.getClass())
+        if (obj == null || getClass() != obj.getClass()) // Check class equality
             return false;
         User user = (User) obj;
-        return username.equals(user.username);
+        return username.equals(user.username); // Compare usernames for equality
     }
 
     @Override
     public int hashCode() {
-        return username.hashCode();
+        return username.hashCode(); 
     }
 
-    // Abstract method to be implemented by subclasses
+    // Abstract method to retrieve the type of user (to be implemented by subclasses)
     public abstract String getUserType();
 }
