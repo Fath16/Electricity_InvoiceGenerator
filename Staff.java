e6d4c6e2ddb46374e8e2cdd33e452cd932dd4645
Fath16 <106072698+Fath16@users.noreply.github.com>
2023-12-26 13:35:22 +0700

    Update Staff.java
    add comment

--- Staff.java
+++ Staff.java
@@ -2,9 +2,10 @@ package Electricity_InvoiceGenerator;
 
 public class Staff extends User {
     public Staff(String username, String password) {
-        super(username, password);
+        super(username, password);    
     }
-
+    
+// Override the abstract method from the User class to specify the user type as "Staff"
     @Override
     public String getUserType() {
         return "Staff";
