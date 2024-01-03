102e38945f8a8449eccbde8c0667f234488e2221
Fath16 <106072698+Fath16@users.noreply.github.com>
2024-01-04 01:22:05 +0700

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,17 +1,34 @@
 package Electricity_InvoiceGenerator;
 
+// Client class inherits from User
 public class Client extends User {
     private String clientID;
+
+    // Constructor for Client
     public Client(String clientID, String username, String password) {
         super(username, password);
         this.clientID = clientID;
     }
+
+    // Getter method for clientID
     public String getClientID() {
         return clientID;
     }
 
+    // Override the abstract method from the User class to specify the user type as "Client"
     @Override
     public String getUserType() {
         return "Client";
+    }
+
+    // Demonstrate method overloading
+    public void printUserInfo() {
+        System.out.println("Client Information: " + toString());
+    }
+
+    // Demonstrate method overriding
+    @Override
+    public String toString() {
+        return super.toString() + ", ClientID: " + clientID;
     }
 }
