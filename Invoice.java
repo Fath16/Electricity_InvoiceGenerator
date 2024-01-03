9266e4f7aa9ee58dbcf1e4e98e6e68e5a79ffbf9
Fath16 <106072698+Fath16@users.noreply.github.com>
2024-01-04 01:22:21 +0700

    Update Invoice.java

--- Invoice.java
+++ Invoice.java
@@ -1,14 +1,17 @@
 package Electricity_InvoiceGenerator;
 
+// Invoice class to represent an invoice
 public class Invoice {
     private String clientUsername;
     private double totalAmount;
 
+    // Constructor for Invoice
     public Invoice(String clientUsername, double totalAmount) {
         this.clientUsername = clientUsername;
         this.totalAmount = totalAmount;
     }
 
+    // Getter methods for Invoice
     public String getClientUsername() {
         return clientUsername;
     }
@@ -17,8 +20,10 @@ public class Invoice {
         return totalAmount;
     }
 
+    // Override toString() to represent invoice information as a string
     @Override
     public String toString() {
         return "Client: " + clientUsername + ", Total Amount: $" + totalAmount;
     }
 }
+
